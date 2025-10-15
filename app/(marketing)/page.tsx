2567d698821e70d36971fa54b138b6d0b6afc41c
Andres Soria <andres.zov@gmail.com>
2025-10-15 12:46:11 -0600

    Add promotional heading to  marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,22 @@
+import { Medal } from 'lucide-react';
+
 const MarketingPage = () => {
-  return <div>Main Marketing Page</div>;
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <div className="flex items-center justify-center flex-col">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase ">
+          <Medal className="h-6 w-6 mr-2" />
+          No. 1 task managment platform
+        </div>
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          Taskify helps you manage your tasks
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+          smarter and faster.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MarketingPage;
