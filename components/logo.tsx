3d649cab3a5e783aa617721cc6f97b05533884d1
Andres Soria <andres.zov@gmail.com>
2025-10-23 20:15:09 -0600

    Fix font path in Logo component

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const headingFont = localFont({
-  src: '../../public/fonts/font.woff2',
+  src: '../public/fonts/font.woff2',
 });
 
 export const Logo = () => {
