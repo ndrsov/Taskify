86f4c2482d41b92e424f93faa88c5b2397d82d61
Andres Soria <andres.zov@gmail.com>
2025-10-23 20:18:37 -0600

    Fix missing return statement in Logo component

--- components/logo.tsx
+++ components/logo.tsx
@@ -8,12 +8,16 @@ const headingFont = localFont({
 });
 
 export const Logo = () => {
-  <Link href="/">
-    <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-      <Image src="/logo.svg" alt="Logo" height={30} width={30} />
-      <p className={cn(headingFont.className, 'text-lg text-neutral-700 pb-1')}>
-        Taskify
-      </p>
-    </div>
-  </Link>;
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <p
+          className={cn(headingFont.className, 'text-lg text-neutral-700 pb-1')}
+        >
+          Taskify
+        </p>
+      </div>
+    </Link>
+  );
 };
