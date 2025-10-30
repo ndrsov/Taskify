f44d6edd9c3d2eb77a34ca990b808811afece42c
Andres Soria <andres.zov@gmail.com>
2025-10-30 11:25:41 -0600

    Fix padding size on both footer and logo component

--- components/logo.tsx
+++ components/logo.tsx
@@ -13,7 +13,7 @@ export const Logo = () => {
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
         <Image src="/logo.svg" alt="Logo" height={30} width={30} />
         <p
-          className={cn(headingFont.className, 'text-lg text-neutral-700 pb-1')}
+          className={cn(headingFont.className, 'text-lg text-neutral-700 pt-1')}
         >
           Taskify
         </p>
