3258df713ca061c792b0c43005a7be07fee988d7
Andres Soria <andres.zov@gmail.com>
2025-11-20 12:13:00 -0600

    Add OrganizationSwitcher and UserButton to Navbar for enhanced user management

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,5 +1,6 @@
 import { Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 import { Plus } from 'lucide-react';
 
 export const Navbar = () => {
@@ -15,9 +16,29 @@ export const Navbar = () => {
         >
           Create
         </Button>
-        <Button size="sm" className="rounded-sm hidden md:block">
+        <Button size="sm" className="rounded-sm block md:hidden">
           <Plus className="h-4 w-4" />
         </Button>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2 ">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          afterSelectOrganizationUrl="/organization/:id"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: 'flex',
+                justifyConetent: 'center',
+                alignItems: 'center',
+              },
+            },
+          }}
+        />
+        <UserButton
+          appearance={{ elements: { avatarBox: { height: 30, width: 30 } } }}
+        />
       </div>
     </nav>
   );
