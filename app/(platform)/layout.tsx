6d6eaba7748540e0b4a288fe80a9019f2cdf4248
Andres Soria <andres.zov@gmail.com>
2025-11-20 12:12:54 -0600

    Enhance PlatformLayout to include afterMultiSessionSingleSignOutUrl for improved session management

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,7 +1,11 @@
 import { ClerkProvider } from '@clerk/nextjs';
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider afterMultiSessionSingleSignOutUrl="/">
+      {children}
+    </ClerkProvider>
+  );
 };
 
 export default PlatformLayout;
